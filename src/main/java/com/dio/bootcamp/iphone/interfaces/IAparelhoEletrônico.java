c70523cfbae78ef11dea79b17f0ebb6f230ca812
FXOliveira <81167481+FXOliveira@users.noreply.github.com>
2024-06-29 15:22:47 -0300

    criação da interface das funcionalidade de um aparelho celular

---
+++ src/main/java/com/dio/bootcamp/iphone/interfaces/IAparelhoEletrônico.java
@@ -0,0 +1,9 @@
+package com.dio.bootcamp.iphone.interfaces;
+
+public interface IAparelhoEletrônico {
+    public void ligar();
+    public void desligar();
+    public void iniciarCorreioVoz();
+    public void alterarVolume();
+    public void atender();
+}
